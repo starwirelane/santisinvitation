51759cbc472dd15e356a1c7ec75aaef65ceedf1a
starwirelane <fa262269@gmail.com>
2026-04-08 19:00:00 -0700

    Update gift card image import path

--- src/pages/Gifts.tsx
+++ src/pages/Gifts.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import fieldTexture from "@/assets/field-texture.jpg";
 import jerseyImg from "@/assets/jersey.jpg";
 import moneyImg from "@/assets/money.jpg";
-import giftcardImg from "@/assets/giftcard.jpg";
+import giftcardImg from "@/assets/gift-cards.jpg";
 
 const fadeUp = {
   hidden: { opacity: 0, y: 20 },
